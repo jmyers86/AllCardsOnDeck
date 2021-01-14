183f3c62e2fcd04f22b63869c9520aad8bd812df
Joseph M. Myers <joecaricature@gmail.com>
2021-01-14 18:33:41 -0500

    just getting started

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AllCardsOnDeck
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            int numberOfCardsInDeck = 52;
+            var suits = new List<string>() { "Clubs", "Hearts", "Spades", "Diamonds" };
+            var faces = new List<string>();
+            var deck = new List<string>();
+
+            for (int suitIndex = 0; suitIndex < suits.Count; suitIndex++)
+            {
+                string suit = suits[suitIndex];
+            }
+
+            for (int faceIndex = 0; faceIndex < 13; faceIndex++)
+                string card;
+
+            switch (faceIndex)
+            {
+
+            }
+
+
+
+
+            
+        }
     }
-  }
 }
